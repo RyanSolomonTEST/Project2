dfcad04f2cfecdcae43559acce893c4ce30ad6b1
RyanSolomonTEST <rsolomon1@ufl.edu>
2019-09-08 15:04:36 -0400

    added database URI to my own config file

---
+++ config.js
@@ -0,0 +1,7 @@
+
+
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://ryanDB:no1knows@bootcamp2-qg8ys.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+  }
+};
