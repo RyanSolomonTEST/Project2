001277df31e38bc990ec72a708e4b03ded07e30e
RyanSolomonTEST <32300835+RyanSolomonTEST@users.noreply.github.com>
2019-09-09 14:11:59 -0400

    Delete config.js
    don't need

--- config.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://ryanDB:no1knows@bootcamp2-qg8ys.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-  }
-};
