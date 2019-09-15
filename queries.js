2add81358cfbcfb988508fd6a31ef237f2abd356
RyanSolomonTEST <32300835+RyanSolomonTEST@users.noreply.github.com>
2019-09-15 11:40:41 -0400

    fixed console.log messages

--- queries.js
+++ queries.js
@@ -1,6 +1,10 @@
 /* Add all the required libraries*/
+var mongoose = require('mongoose');
+var ListingSchema = require('./ListingSchema.js');
+var configFile = require('./config.js');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect(configFile.db.uri, { useNewUrlParser: true });
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,6 +14,13 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   ListingSchema.findOne({name: "Library West"}, function(err, data){
+
+      if(err){
+        throw err;
+      }
+      console.log(data);
+   })
 };
 var removeCable = function() {
   /*
@@ -17,20 +28,41 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  ListingSchema.findOneAndRemove({code: "CABL"}, function(err, data){
+
+    if(err){
+      throw err;
+    }
+    console.log("Deleted document with code CABL")
+  })
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Lab address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  ListingSchema.findOneAndUpdate({name: "Phelps Laboratory"},{address: "1953 Museum Rd, Gainesville, FL 32603"},
+  function(err, data){
+    if(err){
+      throw err;
+    }
+    console.log(data);
+  })
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  ListingSchema.find({}, function(err, data){
+
+    if(err){
+      throw err;
+    }
+    console.log(data);
+  })
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
